13eb233543238d2e272f465e3c1274ddf36d4ad8
SonNC <sona9k15tl@gmail.com>
2023-04-05 17:32:11 +0700

    add more anim

--- common-ui/src/main/java/com/mct/base/ui/transition/NoneFragmentTransition.java
+++ common-ui/src/main/java/com/mct/base/ui/transition/NoneFragmentTransition.java
@@ -3,9 +3,14 @@ package com.mct.base.ui.transition;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.mct.base.ui.R;
+
 public class NoneFragmentTransition implements FragmentTransition {
     @Override
     public void applyTransition(@NonNull FragmentTransaction fragmentTransaction) {
-        fragmentTransaction.setCustomAnimations(0, 0, 0, 0);
+        fragmentTransaction.setCustomAnimations(
+                R.anim.no_anim, R.anim.no_anim,
+                R.anim.no_anim, R.anim.no_anim
+        );
     }
 }
